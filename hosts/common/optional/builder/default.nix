f3cbe4fc1f7e7ca3814137f5bad4474c52a5a81c
akosnad <akos@nadudvari.org>
2024-04-11 22:38:08 +0200

    fix builder root login

--- hosts/common/optional/builder/default.nix
+++ hosts/common/optional/builder/default.nix
@@ -1,7 +1,12 @@
+{ lib, ... }:
 {
   users.users.root = {
     openssh.authorizedKeys.keyFiles = [
       ./builder-common.pub
     ];
+  };
+
+  services.openssh.settings = {
+    PermitRootLogin = lib.mkForce "without-password";
   };
 }
