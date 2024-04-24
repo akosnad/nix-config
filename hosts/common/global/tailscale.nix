4fe4b1adb6b1562f00f5c413032b817e2193142b
akosnad <akos@nadudvari.org>
2024-04-24 10:31:07 +0200

    add tailscale auth key

--- hosts/common/global/tailscale.nix
+++ hosts/common/global/tailscale.nix
@@ -3,7 +3,14 @@
   services.tailscale = {
     enable = true;
     useRoutingFeatures = lib.mkDefault "client";
+    authKeyFile = "/run/secrets/tailscale-auth-key";
   };
+
+  sops.secrets.tailscale-auth-key = {
+    sopsFile = ../secrets.yaml;
+    neededForUsers = true;
+  };
+
   networking.firewall.allowedUDPPorts = [ 41641 ];
 
   networking.hosts = {
