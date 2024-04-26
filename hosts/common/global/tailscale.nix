84e43e4458b2e23607ae047aa45765ba424325df
akosnad <akos@nadudvari.org>
2024-04-27 00:37:10 +0200

    fix tailscale auth key location

--- hosts/common/global/tailscale.nix
+++ hosts/common/global/tailscale.nix
@@ -3,7 +3,7 @@
   services.tailscale = {
     enable = true;
     useRoutingFeatures = lib.mkDefault "client";
-    authKeyFile = "/run/secrets/tailscale-auth-key";
+    authKeyFile = "/run/secrets-for-users/tailscale-auth-key";
   };
 
   sops.secrets.tailscale-auth-key = {
