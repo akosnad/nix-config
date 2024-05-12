3d77969790878d13fb402fdcacf0d5091a8f95a4
akosnad <akos@nadudvari.org>
2024-05-12 13:24:05 +0200

    athena: add swapfile

--- hosts/athena/hardware-configuration.nix
+++ hosts/athena/hardware-configuration.nix
@@ -26,7 +26,11 @@
     fsType = "vfat";
   };
 
-  swapDevices = [ ];
+  swapDevices = [{
+    device = "/.swapfile";
+    size = 64 * 1024;
+  }];
+
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
