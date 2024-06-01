bf8eb5b67998bbe2827f8edc3c33d7cbb0b880a4
akosnad <akos@nadudvari.org>
2024-06-01 22:32:28 +0200

    eww: open swaync when clicking on time widget

--- home/akos/features/desktop/common/wayland/eww/widgets/time.nix
+++ home/akos/features/desktop/common/wayland/eww/widgets/time.nix
@@ -1,6 +1,15 @@
-{ pkgs, ... }: pkgs.writeText "time.yuck" /* yuck */ ''
+{ pkgs, ... }:
+let
+  toggle-notifications = pkgs.writeScriptBin "toggle-notifications" ''
+    ${pkgs.swaynotificationcenter}/bin/swaync-client -t
+  '';
+in
+pkgs.writeText "time.yuck" /* yuck */ ''
   (defwidget time []
-    (label :text "''${time}" :class "time")
+    (eventbox
+      :onclick "${toggle-notifications}/bin/toggle-notifications"
+      (label :text "''${time}" :class "time")
+    )
   )
 
   (defpoll time :interval "10s"
