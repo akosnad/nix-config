6dbee47489282640714239e1c7c290e50fb20f9d
akosnad <akos@nadudvari.org>
2024-06-01 22:59:22 +0200

    add optional powersave config

--- hosts/athena/default.nix
+++ hosts/athena/default.nix
@@ -14,6 +14,7 @@
     ./hardware-configuration.nix
 
     ../common/global
+    ../common/optional/powersave.nix
     ../common/optional/use-builders.nix
     ../common/optional/quietboot.nix
     ../common/optional/secureboot.nix
