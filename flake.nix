b9ed62ea2bf23b5d0ea33465ab2ea4dcabf5194e
akosnad <akos@nadudvari.org>
2024-07-06 14:24:26 +0200

    downgrade home-manager to release-24.05
    this fixes the warning about mismatching nixpkgs and home-manager
    versions.

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/release-24.05";
 
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     nix-index-database = {
