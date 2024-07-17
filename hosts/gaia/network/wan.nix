838136ce9d9a8af1f3a9cdc4a275bd231602d943
akosnad <akos@nadudvari.org>
2024-07-17 23:09:56 +0200

    chore(gaia): mwan routing table with routing rule

--- hosts/gaia/network/wan.nix
+++ hosts/gaia/network/wan.nix
@@ -13,7 +13,7 @@
       };
       bondConfig.Mode = "active-backup";
     };
-    
+
     networks = {
       "30-bond-wan0-bind" = {
         matchConfig.Name = "wan0";
@@ -34,7 +34,26 @@
           DHCP = true;
           IPv6AcceptRA = true;
         };
+        routingPolicyRules = [{
+          routingPolicyRuleConfig = {
+            OutgoingInterface = "bond-wan";
+            Table = 200;
+            Priority = 16384;
+          };
+        }];
+        routes = [{
+          routeConfig = {
+            Table = 200;
+            Destination = "0.0.0.0/0";
+            Protocol = "static";
+            Gateway = "_dhcp4";
+          };
+        }];
       };
+    };
+
+    config.routeTables = {
+      mwan = 200;
     };
   };
 }
