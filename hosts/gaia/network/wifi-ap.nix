6526551a003bc4ce319d2c6908f73e89a411b5b5
akosnad <akos@nadudvari.org>
2024-07-18 21:18:30 +0200

    fix(gaia/net): remove unsupported wifi adatper capabilities

--- hosts/gaia/network/wifi-ap.nix
+++ hosts/gaia/network/wifi-ap.nix
@@ -15,6 +15,7 @@
   services.hostapd = {
     enable = true;
     radios."wifi0" = {
+      wifi4.capabilities = [ ];
       networks."wifi0" = {
         ssid = "Gaia2";
         authentication = {
