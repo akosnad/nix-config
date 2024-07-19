4fbd7e1e36a2de2eb816c5329758449a352fcc97
akosnad <akos@nadudvari.org>
2024-07-19 18:28:20 +0200

    chore(gaia/net/dhcp): set static lease hosts as host file entries

--- hosts/gaia/network/dhcp.nix
+++ hosts/gaia/network/dhcp.nix
@@ -8,14 +8,54 @@ let
     upper = "254";
     leaseTime = "1h";
   };
+  domain = "home.arpa";
+
+  staticLeases = [
+    {
+      mac = "96:5F:26:76:7C:8C";
+      ip = "10.20.0.3";
+      name = "ap1";
+    }
+    {
+      mac = "28:d1:27:be:7b:3d";
+      ip = "10.20.0.5";
+      name = "ap2";
+    }
+    {
+      mac = "3C:CD:57:73:05:22";
+      ip = "10.20.0.6";
+      name = "ap3";
+    }
+    {
+      mac = "52:54:00:64:11:7B";
+      ip = "10.20.0.36";
+      name = "homeassistant";
+    }
+    {
+      mac = "F4:91:1E:F6:E3:A3";
+      ip = "10.20.0.30";
+      name = "helios";
+    }
+    {
+      mac = "A8:5E:45:CD:FC:8A";
+      ip = "10.20.0.96";
+      name = "kratos";
+    }
+    {
+      mac = "2C:27:D7:1F:98:BD";
+      ip = "10.20.0.4";
+      name = "zeus";
+    }
+  ];
+
 in
 {
   services.dnsmasq = {
     enable = true;
     settings = {
       port = "";
-      domain = "home.arpa";
-      local = "/home.arpa/";
+      inherit domain;
+      local = "/${domain}/";
       interface = "br-lan";
       addn-hosts = "/etc/hosts";
       stop-dns-rebind = true;
@@ -40,15 +80,14 @@ in
       ];
 
       # Static leases
-      dhcp-host = [
-        "96:5F:26:76:7C:8C,10.20.0.3,ap1"
-        "28:d1:27:be:7b:3d,10.20.0.5,ap2"
-        "3C:CD:57:73:05:22,10.20.0.6,ap3"
-        "52:54:00:64:11:7B,10.20.0.36,homeassistant"
-        "F4:91:1E:F6:E3:A3,10.20.0.30,helios"
-        "A8:5E:45:CD:FC:8A,10.20.0.96,kratos"
-        "2C:27:D7:1F:98:BD,10.20.0.4,zeus"
-      ];
+      dhcp-host = builtins.map (host: "${host.mac},${host.ip},${host.name}") staticLeases;
     };
   };
+
+  networking.hosts = builtins.listToAttrs (builtins.map
+    (host: {
+      name = host.ip;
+      value = [ "${host.name}.${domain}" host.name ];
+    })
+    staticLeases);
 }
