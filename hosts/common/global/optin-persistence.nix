be39e68c8ddbc925c083738d02cfc2bb6e477411
akosnad <akos@nadudvari.org>
2024-07-20 22:20:09 +0200

    fix(common/optin-persistence): persist nix trusted settings for root
    this fixes the auto upgrade scripts not accepting previously trusted
    settings from this flake.

--- hosts/common/global/optin-persistence.nix
+++ hosts/common/global/optin-persistence.nix
@@ -13,6 +13,9 @@
 
       files = [
         "/etc/machine-id"
+
+        # for system auto upgrading to remember trusted settings in flake
+        "/root/.local/share/nix/trusted-settings.json"
       ];
     };
   };
