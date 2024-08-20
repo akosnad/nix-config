b61484258feef92963915cca5ac3feaa6190b3a5
akosnad <akos@nadudvari.org>
2024-08-20 20:48:37 +0200

    chore(zeus): remove hm corruption dummy fix

--- home/akos/zeus.nix
+++ home/akos/zeus.nix
@@ -4,11 +4,6 @@
     ./global
   ];
 
-  home.file."test.txt".text = "hello";
-  home.sessionVariables = {
-    FOO = "BAR";
-  };
-
   home.persistence."/persist/${config.home.homeDirectory}".directories = [
     "docker"
     "libvirt"
