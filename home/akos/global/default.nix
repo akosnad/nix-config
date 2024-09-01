d8b3047b5179a7277172274594c5bc794033cee5
akosnad <akos@nadudvari.org>
2024-09-01 19:02:13 +0200

    chore: remove and disable nix.fzt.one binary cache

--- home/akos/global/default.nix
+++ home/akos/global/default.nix
@@ -28,14 +28,12 @@ in
         "https://cache.nixos.org/"
         "https://nix-community.cachix.org"
         "https://hyprland.cachix.org"
-        "https://nix.fzt.one/"
       ];
       trusted-public-keys = [
         "akosnad.cachix.org-1:mohKqHWc/aZqkAOWmPfvqRiHmhQ3wQ6R7g9ULwNaRfw="
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-        "nix.fzt.one-1:W6+n+PqYiAINgEUYnAxoDrV0xrjPR0C0fJeIDp3nvAw="
       ];
     };
   };
--- hosts/zeus/default.nix
+++ hosts/zeus/default.nix
@@ -25,7 +25,6 @@
 
     ./libvirt
     ./buildbot-master.nix
-    ./harmonia.nix
   ];
 
   boot.kernelParams = [
