7e2736689f4c7c2bdb898d5f1ee4ce2760c09bbd
akosnad <akos@nadudvari.org>
2024-10-23 14:41:23 +0200

    feat(hosts/common/auto-upgrade): give the service less priority

--- hosts/common/global/auto-upgrade.nix
+++ hosts/common/global/auto-upgrade.nix
@@ -21,34 +21,40 @@ in
 
   # Only run if current config (self) is older than the new one.
   systemd.services.nixos-upgrade = lib.mkIf config.system.autoUpgrade.enable {
-    serviceConfig.ExecCondition = lib.getExe (
-      pkgs.writeShellApplication {
-        name = "check-nixos-upgrade";
-        runtimeInputs = with pkgs; [ curl jq ];
-        text = ''
-          lastModified() {
-            nix flake metadata "$1" --refresh --json | jq '.lastModified'
-          }
+    serviceConfig = {
+      # set a low priority
+      Nice = "15";
+      IOSchedulingPriority = "7";
 
-          checkBuilt() {
-            builder="$(curl -fs "${buildbotApi}/builders" | jq -r ".builders[] | select(.name == \"""$1""\") | .builderid")"
-            # TODO: querying build from revision instead of latest possible?
-            build="$(curl -fs "${buildbotApi}/builders/$builder/builds" | jq -Mcr '.builds | sort_by(.number) | reverse | first')"
-            
-            # check if build start date is not older than last revision
-            test "$(jq -r '.started_at' <<< "$build")" -gt "$upstreamModified"
+      ExecCondition = lib.getExe (
+        pkgs.writeShellApplication {
+          name = "check-nixos-upgrade";
+          runtimeInputs = with pkgs; [ curl jq ];
+          text = ''
+            lastModified() {
+              nix flake metadata "$1" --refresh --json | jq '.lastModified'
+            }
 
-            # check if build has completed and is successful
-            jq -e '.complete and .results == 0' <<< "$build" > /dev/null
-          }
+            checkBuilt() {
+              builder="$(curl -fs "${buildbotApi}/builders" | jq -r ".builders[] | select(.name == \"""$1""\") | .builderid")"
+              # TODO: querying build from revision instead of latest possible?
+              build="$(curl -fs "${buildbotApi}/builders/$builder/builds" | jq -Mcr '.builds | sort_by(.number) | reverse | first')"
 
-          # check if latest upstream config is newer than current
-          upstreamModified="$(lastModified "${config.system.autoUpgrade.flake}")"
-          test "$upstreamModified" -gt "$(lastModified "self")"
+              # check if build start date is not older than last revision
+              test "$(jq -r '.started_at' <<< "$build")" -gt "$upstreamModified"
 
-          # check if system derivation is built upstream (i.e. by CI/CD or available from substituters)
-          checkBuilt "${config.system.autoUpgrade.flake}#checks.${config.nixpkgs.hostPlatform.system}.nixos-${config.networking.hostName}"
-        '';
-      });
+              # check if build has completed and is successful
+              jq -e '.complete and .results == 0' <<< "$build" > /dev/null
+            }
+
+            # check if latest upstream config is newer than current
+            upstreamModified="$(lastModified "${config.system.autoUpgrade.flake}")"
+            test "$upstreamModified" -gt "$(lastModified "self")"
+
+            # check if system derivation is built upstream (i.e. by CI/CD or available from substituters)
+            checkBuilt "${config.system.autoUpgrade.flake}#checks.${config.nixpkgs.hostPlatform.system}.nixos-${config.networking.hostName}"
+          '';
+        });
+    };
   };
 }
