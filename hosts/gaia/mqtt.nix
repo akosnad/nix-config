a2c8c569a0aea2afdfdb5f795fa7185d21d1b57f
akosnad <akos@nadudvari.org>
2024-11-24 22:43:25 +0100

    feat(gaia/mqtt): tls listener

--- hosts/gaia/mqtt.nix
+++ hosts/gaia/mqtt.nix
@@ -1,3 +1,4 @@
+{ config, ... }:
 {
   services.mosquitto = {
     enable = true;
@@ -11,6 +12,34 @@
           allow_anonymous = true;
         };
       }
+      {
+        port = 8883;
+        omitPasswordAuth = true;
+        acl = [ "pattern readwrite #" ];
+        settings = {
+          protocol = "mqtt";
+          require_certificate = true;
+          use_identity_as_username = true;
+          cafile = config.sops.secrets.mosquitto-cafile.path;
+          certfile = config.sops.secrets.mosquitto-certfile.path;
+          keyfile = config.sops.secrets.mosquitto-keyfile.path;
+        };
+      }
     ];
+  };
+
+  networking.firewall.allowedTCPPorts = [ 8883 ];
+
+  sops.secrets.mosquitto-cafile = {
+    sopsFile = ./secrets.yaml;
+    owner = config.systemd.services.mosquitto.serviceConfig.User;
+  };
+  sops.secrets.mosquitto-certfile = {
+    sopsFile = ./secrets.yaml;
+    owner = config.systemd.services.mosquitto.serviceConfig.User;
+  };
+  sops.secrets.mosquitto-keyfile = {
+    sopsFile = ./secrets.yaml;
+    owner = config.systemd.services.mosquitto.serviceConfig.User;
   };
 }
