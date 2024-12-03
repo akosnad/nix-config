9f61cf39ea41b3465d4de84c82bd1519b770ccbe
akosnad <akos@nadudvari.org>
2024-12-03 23:18:21 +0100

    chore(hosts/gaia/postgresql): disable pg_backup
    as of now, only homeassistant long term statistics use the database,
    which isn't worth backing up.

--- hosts/gaia/postgresql.nix
+++ hosts/gaia/postgresql.nix
@@ -1,16 +1,9 @@
-{ pkgs, ... }:
 {
   services.postgresql = {
     enable = true;
-    package = pkgs.postgresql;
     settings = {
       max_connections = "300";
       shared_buffers = "80MB";
     };
-  };
-
-  services.postgresqlBackup = {
-    enable = true;
-    backupAll = true;
   };
 }
