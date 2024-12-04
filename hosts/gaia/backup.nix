863569404b4b63deec56d13d8c7d0b27c82525ee
akosnad <akos@nadudvari.org>
2024-12-04 19:30:29 +0100

    feat(hosts/gaia/backup): backup system persistent data

--- hosts/gaia/backup.nix
+++ hosts/gaia/backup.nix
@@ -1,6 +1,5 @@
 { pkgs, lib, config, ... }:
 let
-  restic-hass-password = config.sops.secrets.restic-hass-password.path;
   hass-token = config.sops.secrets.hass-token.path;
   backupPrepareScript = pkgs.writeShellApplication {
     name = "backup-prepare";
@@ -19,22 +18,53 @@ in
       paths = [ "${config.services.home-assistant.configDir}/backups" ];
       initialize = true;
       repository = "rclone:onedrive-personal:/Backups/gaia-home-assistant";
-      passwordFile = restic-hass-password;
+      passwordFile = config.sops.secrets.restic-hass-password.path;
       rcloneConfigFile = "/etc/rclone.conf";
       backupPrepareCommand = "${lib.getExe backupPrepareScript}";
-      pruneOpts = [ "--keep-last 14" ];
+      pruneOpts = [ "--keep-daily 14" "--keep-weekly 9" ];
       timerConfig = {
         OnCalendar = "*-*-* 23:00:00";
       };
     };
+    gaia-persist = {
+      paths = [
+        "/var/lib/tailscale"
+        "/var/lib/systemd"
+        "/var/lib/private/step-ca"
+        "/var/lib/dnsmasq"
+        "/var/lib/nixos"
+        "/var/lib/mosquitto/mosquitto.db"
+
+        "/etc/ssh/ssh_host_*_key"
+        "/etc/ssh/ssh_host_*_key.pub"
+        "/etc/rclone.conf"
+        "/etc/machine-id"
+      ];
+      exclude = [
+        "/var/lib/tailscale/tailscaled.log*.txt"
+        "/var/lib/systemd/coredump"
+      ];
+      initialize = true;
+      repository = "rclone:onedrive-personal:/Backups/gaia-persist";
+      passwordFile = config.sops.secrets.restic-gaia-persist-password.path;
+      rcloneConfigFile = "/etc/rclone.conf";
+      pruneOpts = [ "--keep-daily 14" "--keep-weekly 9" ];
+      timerConfig = {
+        OnCalendar = "*-*-* 00:10:00";
+      };
+    };
   };
 
-  sops.secrets.restic-hass-password = {
-    sopsFile = ./secrets.yaml;
-    neededForUsers = true;
-  };
-  sops.secrets.hass-token = {
-    sopsFile = ./secrets.yaml;
-    neededForUsers = true;
-  };
+  sops.secrets =
+    let
+      secretsCommonOpts = {
+        sopsFile = ./secrets.yaml;
+      };
+      mkSecrets = names: lib.genAttrs names (_: secretsCommonOpts);
+    in
+    mkSecrets [
+      "restic-hass-password"
+      "restic-gaia-persist-password"
+      "hass-token"
+    ];
 }
