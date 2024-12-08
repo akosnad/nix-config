0d643b6ae04e0d031d2b8cf1039d821205495337
akosnad <akos@nadudvari.org>
2024-12-08 18:53:51 +0100

    fix(hosts/zeus/frigate): don't enforce SSL
    see comment for explanation

--- hosts/zeus/frigate.nix
+++ hosts/zeus/frigate.nix
@@ -6,7 +6,7 @@
     settings = {
       auth.enabled = false;
       # enable restreaming
-      go2rtc = {};
+      go2rtc = { };
       cameras.arges = {
         ffmpeg.inputs = [{
           path = "rtsp://frigate:{FRIGATE_RTSP_PASSWORD}@arges.home.arpa/media/video1";
@@ -18,8 +18,35 @@
 
   services.nginx.virtualHosts.frigate = {
     serverAliases = [ "frigate.home.arpa" ];
-    forceSSL = true;
     enableACME = true;
+
+    # TODO: can't force SSL because Home Assistant doesn't trust the CA
+    # tried: https://nixos.wiki/wiki/Home_Assistant#Trust_a_private_certificate_authority
+    # fails to build some python packages
+    forceSSL = false;
+    enableSSL = true;
+    listen = [
+      {
+        addr = "0.0.0.0";
+        port = 80;
+        ssl = false;
+      }
+      {
+        addr = "0.0.0.0";
+        port = 443;
+        ssl = true;
+      }
+      {
+        addr = "[::]";
+        port = 80;
+        ssl = false;
+      }
+      {
+        addr = "[::]";
+        port = 443;
+        ssl = true;
+      }
+    ];
   };
 
   systemd.services.frigate = {
