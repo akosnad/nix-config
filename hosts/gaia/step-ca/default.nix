df6a226fd7113908e42e0dbe864bcfd30dd8c6bf
akosnad <akos@nadudvari.org>
2024-12-08 21:38:01 +0100

    chore(hosts/gaia/step-ca): increase default cert validity to 14 days

--- hosts/gaia/step-ca/default.nix
+++ hosts/gaia/step-ca/default.nix
@@ -31,8 +31,8 @@
           allowRenewalAfterExpiry = false;
           disableSmallstepExtensions = false;
           minTLSCertDuration = "5m";
-          maxTLSCertDuration = "${toString (14 * 24)}h";
-          defaultTLSCertDuration = "24h";
+          maxTLSCertDuration = "${toString (30 * 24)}h";
+          defaultTLSCertDuration = "${toString (14 * 24)}h";
         };
         options = {
           x509 = { };
