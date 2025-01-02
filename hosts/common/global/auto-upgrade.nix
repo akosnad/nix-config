ef77458b7432cc26907e982c9224ba6c247ffe91
akosnad <akos@nadudvari.org>
2025-01-02 18:51:16 +0100

    feat(hosts/common/auto-upgrade): fetch system path from caches
    this introduces custom logic to the `nixos-upgrade` service. the goal is
    to completely avoid evaluating the system configuration, instead fetch
    the derivation outputs from binary caches. this should succeed, as the
    condition to run the service checks if the path is built on CI.

--- hosts/common/global/auto-upgrade.nix
+++ hosts/common/global/auto-upgrade.nix
@@ -62,6 +62,39 @@ in
             checkBuilt "${config.system.autoUpgrade.flake}#checks.${config.nixpkgs.hostPlatform.system}.nixos-${config.networking.hostName}" "$newRevision"
           '';
         });
+
+      ExecStart = lib.mkForce (lib.getExe (
+        pkgs.writeShellApplication {
+          name = "nixos-upgrade";
+          runtimeInputs = with pkgs; [ curl jq nix ];
+          text = ''
+            revision() {
+              nix flake metadata "$1" --refresh --json | jq -r '.revision'
+            }
+
+            getSystemPath() {
+              # change for given revision
+              change="$(curl -fs "${buildbotApi}/changes?field=revision&field=changeid" | jq -r ".changes[] | select(.revision == \"""$2""\") | .changeid")"
+              # builder for given host
+              builder="$(curl -fs "${buildbotApi}/builders?field=name&field=builderid" | jq -r ".builders[] | select(.name == \"""$1""\") | .builderid")"
+              # latest build for given change and builder
+              build="$(curl -fs "${buildbotApi}/changes/$change/builds?builderid__eq=$builder" | jq -Mcr '.builds | sort_by(.number) | reverse | first')"
+              buildnumber="$(jq -r '.number' <<< "$build")"
+
+              drv="$(curl -fs "${buildbotApi}/builders/$builder/builds/$buildnumber/properties" | jq -r '.properties[0].drv_path[0]')"
+              nix-store --realise "$drv" --no-fallback --max-silent-time 300 --timeout 1800
+            }
+
+            newRevision="$(revision "${config.system.autoUpgrade.flake}")"
+            echo "Fetching system path for revision $newRevision ..."
+            newPath="$(getSystemPath "${config.system.autoUpgrade.flake}#checks.${config.nixpkgs.hostPlatform.system}.nixos-${config.networking.hostName}" "$newRevision")"
+            echo "Registering new system profile for $newPath ..."
+            nix-env --profile /nix/var/nix/profiles/system --set "$newPath"
+            echo "Activating new system profile ..."
+            /nix/var/nix/profiles/system/bin/switch-to-configuration "${config.system.autoUpgrade.operation}"
+            echo "Done."
+          '';
+        }));
     };
   };
 }
