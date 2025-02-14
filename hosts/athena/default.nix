98005e947c19b245b0881387ae688a2308ebf7e4
akosnad <akos@nadudvari.org>
2025-02-14 22:23:34 +0100

    fix({athena,kratos}): open kde-connect ports through firewall

--- hosts/athena/default.nix
+++ hosts/athena/default.nix
@@ -61,6 +61,9 @@
 
   programs.nh.clean.enable = false;
 
+  # needed to open for firewall
+  programs.kdeconnect.enable = true;
+
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "23.11";
 }
