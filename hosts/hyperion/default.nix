58e89ab92f8eef661a5fc9ea28264e796095de93
akosnad <akos@nadudvari.org>
2025-02-23 11:28:23 +0100

    chore(hyperion): init hardware related config

--- hosts/hyperion/default.nix
+++ hosts/hyperion/default.nix
@@ -1,16 +1,25 @@
+{ inputs, ... }:
 {
   imports = [
+    inputs.hardware.nixosModules.common-cpu-intel
+    inputs.hardware.nixosModules.common-gpu-nvidia
+    inputs.hardware.nixosModules.common-pc-ssd
+
+
     ./hardware-configuration.nix
     ./disk-config.nix
 
     ../common/global
     ../common/optional/ephemeral-btrfs.nix
     ../common/optional/secureboot.nix
+    ../common/optional/nvidia.nix
 
     ../common/users/akos
   ];
 
   networking.hostName = "hyperion";
+
+  hardware.nvidia.prime.offload.enable = false;
 
   system.stateVersion = "24.11";
 }
