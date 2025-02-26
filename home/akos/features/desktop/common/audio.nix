67500b2886945a0ff917295e71602826b356ea4d
akosnad <akos@nadudvari.org>
2025-02-26 13:07:12 +0100

    fix(home/desktop/common/audio): persist wireplumber state

--- home/akos/features/desktop/common/audio.nix
+++ home/akos/features/desktop/common/audio.nix
@@ -1,4 +1,6 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
   home.packages = with pkgs; [ pavucontrol ];
+
+  home.persistence."/persist/${config.home.homeDirectory}".directories = [ ".local/state/wireplumber" ];
 }
