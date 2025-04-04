307d1d4ca871aa657a03ad0e11a22afe99064756
akosnad <akos@nadudvari.org>
2025-04-04 22:11:50 +0200

    feat(home/desktop/hyprlock): display battery info

--- home/akos/features/desktop/common/wayland/hyprlock.nix
+++ home/akos/features/desktop/common/wayland/hyprlock.nix
@@ -1,7 +1,35 @@
-{ config, ... }:
+{ pkgs, lib, config, ... }:
 let
   primaryMonitor = builtins.head (builtins.filter (m: m.primary) config.monitors);
   fontFamily = config.fontProfiles.regular.family;
+
+  batteryInfo = pkgs.writeShellApplication {
+    name = "hyprlock-battery-info";
+    runtimeInputs = with pkgs; [ upower ];
+    text = ''
+      BAT="$(upower -e | grep battery_BAT | head -n1)"
+      if [[ "$BAT" == "" ]]; then
+        echo
+        exit 0
+      fi
+      DATA="$(upower -i "$BAT")"
+
+      percentage="$(echo "$DATA" | grep 'percentage:' | awk '{print $2}')"
+      state="$(echo "$DATA" | grep 'state:' | awk '{print $2}')"
+
+      charging=("󰢟 " "󰢜 " "󰂆 " "󰂇 " "󰂈 " "󰢝 " "󰂉 " "󰢞 " "󰂊 " "󰂋 " "󰂅 ")
+      discharging=("󰂎 " "󰁺 " "󰁻 " "󰁼 " "󰁽 " "󰁾 " "󰁿 " "󰂀 " "󰂁 " "󰂂 " "󰁹 ")
+      pct="$(echo "$percentage" | cut -f1 -d'%')"
+      icon_index=$((pct / 10))
+      if [[ "$state" == "charging" ]]; then
+        icon="''${charging[$icon_index]}"
+      else
+        icon="''${discharging[$icon_index]}"
+      fi
+
+      printf "%s\n" "$icon $percentage"
+    '';
+  };
 in
 {
   programs.hyprlock = {
@@ -41,6 +69,15 @@ in
           valign = "center";
           halign = "center";
           text = "cmd[update:500] date +'%b %d. %A'";
+        }
+        {
+          monitor = primaryMonitor.name;
+          font_size = 12;
+          font_family = fontFamily;
+          position = "0, -72";
+          valign = "center";
+          halign = "center";
+          text = "cmd[update:30000] ${lib.getExe batteryInfo}";
         }
       ];
     };
