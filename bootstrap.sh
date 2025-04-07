ef056085f1543a9a8ac54b594e214c173de6e21c
akosnad <akos@nadudvari.org>
2025-04-08 01:21:52 +0200

    feat(bootstrap.sh): legacy boot (hybrid MBR + GRUB) support
    also moved `nixos-generate-config` logic to be handled by
    `nixos-anywhere`. so during the `populate_config`
    phase, we generate a placeholder in place of the hardware
    config to be filled out later by `nixos-anywhere`.

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,9 +6,12 @@ set -o errexit
 hostname="$1"
 target_host="$2"
 target_disk="$3"
+boot_mode="$4"
 
 if [[ $target_host == "" || $hostname == "" || $target_disk == "" ]]; then
-  echo "Usage: $0 <hostname> <target_host> <target_disk>"
+  echo "Usage: $0 <hostname> <target_host> <target_disk> [boot_mode]"
+  echo
+  echo "boot_mode is one of 'efi' or 'bios'. defaults to 'efi'"
   echo
   echo "Prerequisites:"
   echo "- boot the target machine to a nixos installer"
@@ -16,7 +19,59 @@ if [[ $target_host == "" || $hostname == "" || $target_disk == "" ]]; then
   exit 1
 fi
 
-populate_config() {
+if [ -n "$boot_mode" ]; then
+  if [[ $boot_mode != "bios" && $boot_mode != "efi" ]]; then
+    echo "boot_mode is invalid. allowed values: efi, bios."
+    exit 1
+  fi
+fi
+
+populate_config_common() {
+  mkdir -p "./home/akos"
+
+  if [ -f "./home/akos/${hostname}.nix" ]; then
+    echo "./home/akos/${hostname}.nix already exists, refusing to overwrite."
+    exit 1
+  fi
+
+  cat >"./home/akos/${hostname}.nix" <<EOF
+{
+  imports = [
+    ./global
+  ];
+}
+EOF
+
+  machine="$(_target uname -m)"
+  platform="$(_target uname -s)"
+  if [[ $platform != "Linux" ]]; then
+    echo Host platform "$platform" is not suported, refusing to continue.
+    exit 1
+  fi
+  hostPlatform="${machine}-linux"
+  cat >"./hosts/${hostname}/hardware-configuration.nix" <<EOF
+# This is a placeholder until nixos-anywhere generates this for us.
+{
+  nixpkgs.hostPlatform = "${hostPlatform}";
+}
+EOF
+
+  ssh_key="ssh_host_ed25519_key"
+  ssh-keygen -t ed25519 -N '' -C "${hostname}" -f "./hosts/${hostname}/$ssh_key"
+
+  if yq -e ".keys.hosts | filter(. | anchor == \"${hostname}\") | .[]" .sops.yaml &>/dev/null; then
+    echo "host age key for ${hostname} already present in .sops.yaml, refusing to overwrite."
+    exit 1
+  fi
+
+  age_key="$(ssh-to-age -i "./hosts/${hostname}/${ssh_key}.pub")"
+  printf "Generated system age key:\n\n%s\n\n" "$age_key"
+  yq -e ".keys.hosts += (\"${age_key}\" | . anchor = \"${hostname}\")" -i .sops.yaml
+  yq -e "with(.creation_rules[]; . | select(.path_regex == \"hosts/common/secrets.ya?ml\$\") | .key_groups.[0].age += (\"dummy\" | . alias = \"${hostname}\"))" -i .sops.yaml
+  sops -i -r --add-age "${age_key}" hosts/common/secrets.yaml
+}
+
+populate_config_efi() {
 
   mkdir -p "./hosts/$hostname"
 
@@ -88,37 +143,89 @@ EOF
   fileSystems."/persist".neededForBoot = true;
 }
 EOF
+}
 
-  mkdir -p "./home/akos"
+populate_config_bios() {
 
-  if [ -f "./home/akos/${hostname}.nix" ]; then
-    echo "./home/akos/${hostname}.nix already exists, refusing to overwrite."
-    exit 1
-  fi
+  mkdir -p "./hosts/$hostname"
 
-  cat >"./home/akos/${hostname}.nix" <<EOF
-{ ... }:
+  cat >"./hosts/$hostname/default.nix" <<EOF
+{ lib, ... }:
 {
   imports = [
-    ./global
+    ./hardware-configuration.nix
+    ./disk-config.nix
+
+    ../common/global
+    ../common/optional/ephemeral-btrfs.nix
+
+    ../common/users/akos
   ];
+
+  networking.hostName = "${hostname}";
+
+  boot.loader = {
+    systemd-boot.enable = lib.mkForce false;
+    grub.enable = lib.mkForce true;
+  };
+
+  system.stateVersion = "24.11";
 }
 EOF
 
-  _target nixos-generate-config --no-filesystems --show-hardware-config >"./hosts/${hostname}/hardware-configuration.nix"
-  ssh_key="ssh_host_ed25519_key"
-  ssh-keygen -t ed25519 -N '' -C "${hostname}" -f "./hosts/${hostname}/$ssh_key"
+  cat >"./hosts/$hostname/disk-config.nix" <<EOF
+{ inputs, ... }:
+{
+  imports = [ inputs.disko.nixosModules.disko ];
 
-  if yq -e ".keys.hosts | filter(. | anchor == \"${hostname}\") | .[]" .sops.yaml &>/dev/null; then
-    echo "host age key for ${hostname} already present in .sops.yaml, refusing to overwrite."
-    exit 1
-  fi
+  disko.devices = {
+    disk = {
+      main = {
+        type = "disk";
+        device = "${target_disk}";
+        content = {
+          type = "gpt";
+          partitions = {
+            boot = {
+              size = "1M";
+              type = "EF02"; # for GRUB MBR
+            };
 
-  age_key="$(ssh-to-age -i "./hosts/${hostname}/${ssh_key}.pub")"
-  printf "Generated system age key:\n\n%s\n\n" "$age_key"
-  yq -e ".keys.hosts += (\"${age_key}\" | . anchor = \"${hostname}\")" -i .sops.yaml
-  yq -e "with(.creation_rules[]; . | select(.path_regex == \"hosts/common/secrets.ya?ml\$\") | .key_groups.[0].age += (\"dummy\" | . alias = \"${hostname}\"))" -i .sops.yaml
-  sops -i -r --add-age "${age_key}" hosts/common/secrets.yaml
+            ESP = {
+              name = "ESP";
+              size = "512M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
+              };
+            };
+
+            root = {
+              size = "100%";
+              content = {
+                type = "btrfs";
+                mountOptions = [ "noatime" "compress=zstd" "space_cache=v2" "discard" ];
+                subvolumes = {
+                  "@root" = { mountpoint = "/"; };
+                  "@old_roots" = { };
+                  "@nix" = { mountpoint = "/nix"; };
+                  "@persist" = { mountpoint = "/persist"; };
+                  "@swap" = { mountpoint = "/swap"; };
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+
+  fileSystems."/persist".neededForBoot = true;
+}
+EOF
 }
 
 install_system() {
@@ -127,7 +234,7 @@ install_system() {
   install -d -m755 "$temp/persist/etc/ssh"
   cp "./hosts/${hostname}/ssh_host_ed25519_key" "./hosts/${hostname}/ssh_host_ed25519_key.pub" "$temp/persist/etc/ssh"
 
-  nix run github:nix-community/nixos-anywhere -- --extra-files "$temp" --flake ".#${hostname}" --target-host "root@${target_host}"
+  nix run github:nix-community/nixos-anywhere -- --extra-files "$temp" --generate-hardware-config nixos-generate-config "./hosts/${hostname}/hardware-configuration.nix" --flake ".#${hostname}" --target-host "root@${target_host}"
 
   shred -u "./hosts/${hostname}/ssh_host_ed25519_key"
 
@@ -153,7 +260,12 @@ if [[ $_config_hostname == "$hostname" ]]; then
   echo "Configuration for $hostname seems to already exist, starting installation..."
   install_system
 else
-  populate_config
+  if [[ $boot_mode == "bios" ]]; then
+    populate_config_bios
+  else
+    populate_config_efi
+  fi
+  populate_config_common
   echo "Configuration for ${hostname} has been generated."
   echo
   echo "Now is a good time to make adjustments to it."
