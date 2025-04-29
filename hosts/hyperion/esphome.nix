4ad22112af8162834db0ebcbe2b642ca97ab8366
akosnad <akos@nadudvari.org>
2025-04-29 14:11:09 +0200

    fix(hyperion/esphome): nginx proxy working

--- hosts/hyperion/esphome.nix
+++ hosts/hyperion/esphome.nix
@@ -2,17 +2,23 @@
 {
   services.esphome = {
     enable = true;
-    address = config.devices."${config.networking.hostName}".ip;
+    address = "0.0.0.0";
   };
 
   services.nginx.virtualHosts."${config.networking.hostName}".locations."/esphome" = {
     extraConfig = /* nginx */ ''
+      rewrite ^(/esphome)$ $1/ permanent;
       rewrite /esphome/(.*) /$1 break;
-      proxy_pass http://127.0.0.1:6052;
+      proxy_pass http://127.0.0.1:${toString config.services.esphome.port};
+      proxy_set_header Host $host;
+      proxy_redirect http:// https://;
+      proxy_http_version 1.1;
+      proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+      proxy_set_header Upgrade $http_upgrade;
+      proxy_set_header Connection $connection_upgrade;
+      proxy_set_header X-Forwarded-Host $http_host;
     '';
   };
-
-  networking.firewall.allowedTCPPorts = [ 6052 ];
 
   environment.persistence."/persist".directories = [{
     directory = "/var/lib/private/esphome";
