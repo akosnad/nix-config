5ce6be4489629f2bd351222c84d88c822ba33a07
akosnad <akos@nadudvari.org>
2025-05-07 20:37:36 +0200

    feat(ceyx/headscale): embedded DERP server

--- hosts/ceyx/headscale.nix
+++ hosts/ceyx/headscale.nix
@@ -27,11 +27,19 @@ in
         };
         search_domains = [ "home.arpa" ];
       };
-      derp.server = {
-        enabled = true;
-        region_code = "fzt";
-        region_name = "ts.${baseDomain} Embedded DERP";
-        stun_listen_addr = "0.0.0.0:${toString stunServerPort}";
+      derp = {
+        # disable other DERP servers
+        urls = [ ];
+        server = {
+          enabled = true;
+          region_id = 999;
+          region_code = "fzt";
+          region_name = "ts.${baseDomain} Embedded DERP";
+
+          # we have to leave out the host part to listen on both ipv4 and ipv6
+          # reference: https://github.com/golang/go/issues/9334#issuecomment-67098831
+          stun_listen_addr = ":${toString stunServerPort}";
+        };
       };
     };
     policy = {
@@ -47,14 +55,41 @@ in
   };
 
   networking.hosts."127.0.0.1" = [ "ts.${baseDomain}" ];
-  networking.firewall.allowedUDPPorts = [ stunServerPort ];
+  networking.nftables.enable = true;
+  networking.firewall = {
+    allowedUDPPorts = [ stunServerPort ];
+    checkReversePath = "loose";
+
+    # DERP requires that all ICMP packet types are
+    # allowed. reference: https://tailscale.com/kb/1118/custom-derp-servers
+    allowPing = true;
+    extraInputRules = ''
+      ip protocol icmp accept
+      ip6 nexthdr ipv6-icmp accept
+    '';
+    extraForwardRules = ''
+      ip protocol icmp accept
+      ip6 nexthdr ipv6-icmp accept
+    '';
+  };
 
   services.nginx.virtualHosts."ts.${baseDomain}" = {
     forceSSL = true;
     enableACME = true;
     locations."/" = {
-      proxyPass = "http://${config.services.headscale.address}:${toString config.services.headscale.port}";
-      proxyWebsockets = true;
+      extraConfig = /* nginx */ ''
+        proxy_pass http://${config.services.headscale.address}:${toString config.services.headscale.port};
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection $connection_upgrade;
+        proxy_set_header Host $server_name;
+        proxy_redirect http:// https://;
+        proxy_buffering off;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+        add_header Strict-Transport-Security "max-age=15552000; includeSubDomains" always;
+      '';
     };
   };
 
