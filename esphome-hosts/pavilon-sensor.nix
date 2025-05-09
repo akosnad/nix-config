11e606066369531399dae273e81118a7f2c1cb83
akosnad <akos@nadudvari.org>
2025-05-09 18:47:43 +0200

    fix(esphome-hosts/pavilon-sensor): correct pin labels

--- esphome-hosts/pavilon-sensor.nix
+++ esphome-hosts/pavilon-sensor.nix
@@ -3,8 +3,8 @@
   settings = {
     esp32.board = "wemos_d1_mini32";
     i2c = [{
-      sda = "IO2";
-      scl = "IO4";
+      sda = "GPIO2";
+      scl = "GPIO4";
       scan = true;
     }];
     sensor = with (common.sensorPresets "Pavilon"); [
