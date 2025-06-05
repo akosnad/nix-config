1d36eb73043e393405a731a2d95c46ff1cdd2c8d
akosnad <akos@nadudvari.org>
2025-06-05 15:30:40 +0200

    feat(home/features): add kicad

---
+++ home/akos/features/kicad.nix
@@ -0,0 +1,9 @@
+{ pkgs, config, ... }:
+{
+  home.packages = with pkgs; [ kicad ];
+
+  home.persistence."/persist/${config.home.homeDirectory}".directories = [
+    ".cache/kicad"
+    ".local/share/kicad"
+  ];
+}
