9241e95e17999788659c45a81a51dce92120f358
akosnad <akos@nadudvari.org>
2025-10-14 22:05:37 +0200

    feat(hosts/globa/udev): disable autosuspend on wireless microsoft keyboard + disable wakeup for mouse

--- hosts/common/global/udev/default.nix
+++ hosts/common/global/udev/default.nix
@@ -11,5 +11,9 @@
     # Logitech mouse
     ACTION=="add", ATTRS{idVendor}=="046d", ATTRS{idProduct}=="c53f", ATTR{power/control}="on"
     ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="046d", ATTR{idProduct}=="c53f", ATTR{power/autosuspend}="-1"
+    ACTION=="add", SUBSYSTEM=="usb", DRIVERS=="usb", ATTRS{idVendor}=="046d", ATTRS{idProduct}=="c53f", ATTR{power/wakeup}="disabled"
+    # Microsoft wireless keyboard/touchpad
+    ACTION=="add", ATTRS{idVendor}=="045e", ATTRS{idProduct}=="0800", ATTR{power/control}="on"
+    ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="045e", ATTR{idProduct}=="0800", ATTR{power/autosuspend}="-1"
   '';
 }
