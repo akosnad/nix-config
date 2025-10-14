a42a0cad8112b9c81ad2eab8fa68d6eaa875de68
akosnad <akos@nadudvari.org>
2025-10-14 21:04:17 +0200

    chore(home/kratos): add aux monitor config

--- home/akos/kratos.nix
+++ home/akos/kratos.nix
@@ -19,33 +19,49 @@
 
   specialisation.light.configuration.stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/equilibrium-gray-light.yaml";
 
-  monitors = [
-    {
-      # Samsung
-      name = "DP-2";
-      model = "U28E850";
-      width = 3840;
-      height = 2160;
-      scale = 1.5;
-      x = 0;
-      workspace = "1";
-      primary = true;
-      vrr = 1;
-    }
-    {
-      # Asus
-      name = "HDMI-A-1";
-      model = "ASUS VN247";
-      width = 1920;
-      height = 1080;
-      x = -1920;
-      workspace = "2";
-    }
-    {
-      name = "Unknown-1";
-      enabled = false;
-    }
-  ];
+  monitors =
+    let
+      primary =
+        {
+          name = "DP-2";
+          model = "U28E850";
+          width = 3840;
+          height = 2160;
+          scale = 1.5;
+          x = 0;
+          workspace = "1";
+          primary = true;
+          vrr = 1;
+        };
+      secondary =
+        rec {
+          name = "HDMI-A-1";
+          model = "ASUS VN247";
+          width = 1920;
+          height = 1080;
+          x = -width;
+          workspace = "2";
+        };
+      aux =
+        {
+          name = "HDMI-A-2";
+          model = "LF24T35";
+          width = 1920;
+          height = 1080;
+          x = builtins.floor (primary.width / primary.scale);
+          workspace = "3";
+          vrr = 1;
+        };
+    in
+    [
+      primary
+      secondary
+      aux
+      {
+        name = "Unknown-1";
+        enabled = false;
+      }
+    ];
 
   home.packages = with pkgs; [
     gimp3
