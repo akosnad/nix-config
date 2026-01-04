9334f7347a0fcd05c5f35dd638e8c4c14a197605
akosnad <akos@nadudvari.org>
2026-01-04 11:23:58 +0100

    fix(home/shell/zsh): pass arguments correctly to `t` alias

--- home/akos/features/shell/zsh.nix
+++ home/akos/features/shell/zsh.nix
@@ -7,7 +7,7 @@
     };
     shellAliases = {
       tree = "eza -l --tree";
-      t = "eza -al --tree --git-ignore --git-repos --color=always --icons=always | less -rF";
+      t = "(){ eza -al --tree --git-ignore --git-repos --color=always --icons=always $@ | less -rF }";
       y = "yazi";
       nd = "nix develop -c $SHELL";
     };
