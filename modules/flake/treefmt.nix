babc35a0f2834ceb6bb4523a024005786c7fc1ff
akosnad <akos@nadudvari.org>
2026-04-10 09:58:40 +0200

    chore(flake/checks): only run treefmt on `x86_64-linux`
    on buildbot, aarch64-linux is an emulated platform, and
    treefmt runs a bit slow because of a lot of IO operations.
    this change makes the treefmt flake check only run on the
    native system.

--- modules/flake/treefmt.nix
+++ modules/flake/treefmt.nix
@@ -1,13 +1,24 @@
+{ lib, ... }:
 {
-  perSystem.treefmt = {
-    projectRootFile = "flake.nix";
-    programs = {
-      nixpkgs-fmt.enable = true;
-      rustfmt.enable = true;
-      statix.enable = true;
-      deadnix.enable = true;
-      shfmt.enable = true;
-      black.enable = true;
+  perSystem = { system, config, ... }: {
+    treefmt = {
+      projectRootFile = "flake.nix";
+      programs = {
+        nixpkgs-fmt.enable = true;
+        rustfmt.enable = true;
+        statix.enable = true;
+        deadnix.enable = true;
+        shfmt.enable = true;
+        black.enable = true;
+      };
+    };
+
+    # treefmt is slow on emulated aarch64-linux,
+    # so this disables that check
+    treefmt.flakeCheck = false;
+    checks = lib.mkIf (system == "x86_64-linux") {
+      # source: https://github.com/numtide/treefmt-nix/blob/790751ff7fd3801feeaf96d7dc416a8d581265ba/flake-module.nix#L74
+      treefmt = config.treefmt.build.check config.treefmt.projectRoot;
     };
   };
 }
