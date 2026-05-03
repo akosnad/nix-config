7a68f33edade01c869491a3b057dfbf017f31ba0
akosnad <akos@nadudvari.org>
2026-05-03 19:29:42 +0200

    chore(gaia/mqtt): remove non-tls external endpoint

--- modules/hosts/gaia/mqtt.nix
+++ modules/hosts/gaia/mqtt.nix
@@ -8,6 +8,16 @@
       };
       mkMosquittoSecrets = names:
         lib.genAttrs (map (name: "mosquitto-${name}") names) (_name: mkMosquittoSecret);
+
+      tls = {
+        protocol = "mqtt";
+        allow_anonymous = false;
+        require_certificate = true;
+        use_identity_as_username = true;
+        cafile = config.sops.secrets.mosquitto-cafile.path;
+        certfile = config.sops.secrets.mosquitto-certfile.path;
+        keyfile = config.sops.secrets.mosquitto-keyfile.path;
+      };
     in
     {
       services.mosquitto = {
@@ -26,14 +36,7 @@
             port = 8883;
             omitPasswordAuth = true;
             acl = [ "pattern readwrite #" ];
-            settings = {
-              protocol = "mqtt";
-              require_certificate = true;
-              use_identity_as_username = true;
-              cafile = config.sops.secrets.mosquitto-cafile.path;
-              certfile = config.sops.secrets.mosquitto-certfile.path;
-              keyfile = config.sops.secrets.mosquitto-keyfile.path;
-            };
+            settings = tls;
           }
           {
             port = 8084;
@@ -44,40 +47,15 @@
               allow_anonymous = true;
             };
           }
-          {
-            port = 30160;
-            omitPasswordAuth = false;
-            acl = [
-              "pattern readwrite vili/#"
-              "pattern readwrite homeassistant/device_tracker/vili_tracker/config"
-              "pattern readwrite homeassistant/binary_sensor/vili_ignition/config"
-              "pattern readwrite homeassistant/binary_sensor/vili_battery_charging/config"
-              "pattern readwrite homeassistant/sensor/vili_battery_voltage/config"
-              "pattern readwrite homeassistant/sensor/vili_battery_charge_current/config"
-              "pattern readwrite homeassistant/sensor/vili_ext_voltage/config"
-              "pattern readwrite homeassistant/sensor/vili_ext_current/config"
-              "pattern readwrite homeassistant/sensor/vili_int_temperature/config"
-              "pattern readwrite homeassistant/sensor/vili_satellite_count/config"
-              "pattern readwrite homeassistant/button/vili_reboot/config"
-            ];
-            users.vili = {
-              passwordFile = config.sops.secrets.mosquitto-vili-password.path;
-            };
-            settings = {
-              protocol = "mqtt";
-              allow_anonymous = false;
-            };
-          }
         ];
       };
 
-      networking.firewall.allowedTCPPorts = [ 8883 30160 ];
+      networking.firewall.allowedTCPPorts = [ 8883 ];
 
       sops.secrets = mkMosquittoSecrets [
         "cafile"
         "certfile"
         "keyfile"
-        "vili-password"
       ];
     };
 }
