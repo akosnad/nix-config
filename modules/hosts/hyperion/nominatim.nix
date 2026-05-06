0edfb82db429ebda6fa44879ceb91122b32e95b4
akosnad <akos@nadudvari.org>
2026-05-06 22:52:11 +0200

    feat(hyperion/nominatim): automatic replication

--- modules/hosts/hyperion/nominatim.nix
+++ modules/hosts/hyperion/nominatim.nix
@@ -1,5 +1,6 @@
+{ lib, ... }:
 {
-  flake.modules.nixos."hosts/hyperion" = {
+  flake.modules.nixos."hosts/hyperion" = { config, pkgs, ... }: {
     services.nominatim = {
       enable = true;
       hostName = "nominatim.home.arpa";
@@ -17,13 +18,40 @@
 
         NOMINATIM_FLATNODE_FILE = "/var/lib/nominatim/flatnode/flatnode.bin";
 
-        NOMINATIM_REPLICATION_URL = "https://planet.openstreetmap.org/replication/minute";
-        NOMINATIM_REPLICATION_MAX_DIFF = "100";
+        NOMINATIM_REPLICATION_URL = "https://download.geofabrik.de/europe/hungary-updates";
+        NOMINATIM_REPLICATION_MAX_DIFF = "500";
+        NOMINATIM_REPLICATION_UPDATE_INTERVAL = "86400";
+        NOMINATIM_REPLICATION_RECHECK_INTERVAL = "900";
       };
       ui.config = /* js */ ''
         Nominatim_Config.Page_Title='nominatim.home.arpa';
         Nominatim_Config.Nominatim_API_Endpoint='https://nominatim.home.arpa/';
       '';
+    };
+    systemd.services.nominatim-updates = {
+      environment = (lib.removeAttrs config.systemd.services.nominatim.environment [ "PATH" "PYTHONPATH" ]) // {
+        PYTHONPATH =
+          let
+            extraPackages = pkgs.python3.withPackages (ps: with ps; [ pyosmium ]);
+          in
+          "${extraPackages}/lib/${extraPackages.passthru.libPrefix}/site-packages:" + config.systemd.services.nominatim.environment.PYTHONPATH;
+      };
+      script = ''${pkgs.nominatim}/bin/nominatim replication --once'';
+      wantedBy = [ "multi-user.target" ];
+      serviceConfig = {
+        Type = "simple";
+        User = "nominatim";
+        Group = "nominatim";
+        WorkingDirectory = "/var/lib/nominatim";
+      };
+    };
+    systemd.timers.nominatim-updates = {
+      wantedBy = [ "multi-user.target" ];
+      timerConfig = {
+        OnActiveSec = 30;
+        OnUnitActiveSec = "1min";
+        Unit = "nominatim-updates.service";
+      };
     };
 
     services.postgresql.settings = {
