70f43de2410945cb9529aad5b314ebb7140db20b
akosnad <akos@nadudvari.org>
2026-06-01 10:19:38 +0200

    chore(desktop/glide/ublock): update filter rules

--- modules/desktop/glide/ublock.nix
+++ modules/desktop/glide/ublock.nix
@@ -95,11 +95,14 @@
 
             "www.donpepe.hu##.swal2-backdrop-show.swal2-center.swal2-container"
 
-            "www.patreon.com###transcend-consent-manager"
-
             "bugzilla.mozilla.org##.is-visible.moz-consent-banner"
 
             "stackoverflow.com###onetrust-consent-sdk"
+
+            # cookie consent popups
+            "/.*/###onetrust-consent-sdk"
+            "/.*/###transcend-consent-manager"
+            "www.reddit.com##rpl-modal-card"
           ];
         };
       };
